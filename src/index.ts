f07aebc86d4bcf5a4b1f009b8d538ba2ccbca034
Seif Ahmed <seif.ahmed@softwire.com>
2022-10-18 12:19:01 +0100

    ADD: ListAll function

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
 import { parseTransactionsCSV } from "./csv";
+import { listAll } from "./list";
+import { Transaction } from "./transactions";
+import { UserAccountStore } from "./users";
 
 function collapseTransactions(path: string) {
-	const { transactions, users } = parseTransactionsCSV(path);
+	const { users, transactions } = parseTransactionsCSV(path);
 
 	transactions.forEach((transaction) => {
 		users[transaction.from].handleTransaction(transaction);
 		users[transaction.to].handleTransaction(transaction);
 	});
 
-	return { transactions, users };
+	return { users, transactions };
+}
+
+function listUserTransactions(
+	name: string,
+	users: UserAccountStore,
+	transactions: Transaction[]
+) {
+	const user = users[name];
 }
 
 function main() {
-	collapseTransactions("./examples/Transactions2014.csv");
+	const { users, transactions } = collapseTransactions(
+		"./examples/Transactions2014.csv"
+	);
+
+	listAll(users);
 }
 
 main();
