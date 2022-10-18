7ce57a590a5b4ce5f17f9e500e6aaed0478aa339
Seif Ahmed <seif.ahmed@softwire.com>
2022-10-18 11:46:16 +0100

    ADD: Collapsing transactions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { parseTransactionsCSV } from "./csv";
 
-const { transactions, users } = parseTransactionsCSV(
-	"./examples/Transactions2014.csv"
-);
+function collapseTransactions() {
+	const { transactions, users } = parseTransactionsCSV(
+		"./examples/Transactions2014.csv"
+	);
 
-console.log(transactions);
+	transactions.forEach((transaction) => {
+		users[transaction.from].handleTransaction(transaction);
+		users[transaction.to].handleTransaction(transaction);
+	});
+
+	console.log(users);
+}
+
+collapseTransactions();
--- src/users.ts
+++ src/users.ts
@@ -1,3 +1,5 @@
+import { Transaction } from "./transactions";
+
 export interface UserAccountStore {
 	[name: string]: UserAccount;
 }
@@ -14,5 +16,23 @@ export class UserAccount {
 
 	constructor(private _name: string) {
 		this._balance = 0;
+	}
+
+	addBalance(number: number) {
+		this._balance += number;
+	}
+
+	removeBalance(number: number) {
+		this._balance -= number;
+	}
+
+	handleTransaction(transaction: Transaction) {
+		if (transaction.from === this.name) {
+			this.removeBalance(transaction.amount);
+		}
+
+		if (transaction.to === this.name) {
+			this.addBalance(transaction.amount);
+		}
 	}
 }
