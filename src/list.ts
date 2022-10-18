e5dd03a5779414075d651069982cd43cbbe88303
Seif Ahmed <seif.ahmed@softwire.com>
2022-10-18 12:39:31 +0100

    REFACTOR: Table class

--- src/list.ts
+++ src/list.ts
@@ -1,23 +1,5 @@
 import { UserAccountStore } from "./users";
-
-function getMaxLength(originalList: string[]) {
-	if (originalList.length < 0) {
-		return 0;
-	}
-
-	const list = [...originalList];
-
-	return list.sort((a, b) => b.length - a.length)[0].length;
-}
-
-function getMaxLengthsOfColumn(
-	objectList: { [key: string]: string }[],
-	headers: { [key: string]: string },
-	key: string
-) {
-	const values = objectList.map((o) => o[key]);
-	return getMaxLength([...values, headers[key]]);
-}
+import { Table } from "./table";
 
 export function listAll(users: UserAccountStore): void {
 	const userList = Object.values(users).map((user) => ({
@@ -30,26 +12,7 @@ export function listAll(users: UserAccountStore): void {
 		balance: "Balance (£)",
 	};
 
-	const maxLengths = {
-		name: getMaxLengthsOfColumn(userList, headers, "name"),
-		balance: getMaxLengthsOfColumn(userList, headers, "balance"),
-	};
+	const table = new Table(headers, userList);
 
-	console.log(
-		`${headers.name.padEnd(maxLengths.name)} | ${headers.balance.padEnd(
-			maxLengths.balance
-		)}`
-	);
-
-	const borderName = new Array(maxLengths.name).fill("=").join("");
-	const borderBalance = new Array(maxLengths.balance).fill("=").join("");
-
-	console.log(`${borderName} | ${borderBalance}`);
-
-	userList.forEach((user) => {
-		const name = user.name.padEnd(maxLengths.name);
-		const balance = user.balance.padStart(maxLengths.balance);
-
-		console.log(`${name} | ${balance}`);
-	});
+	table.printTable();
 }
---
+++ src/table.ts
@@ -0,0 +1,41 @@
+export class Table<T> {
+	constructor(
+		private labels: { [column in keyof T]: string },
+		private data: T[]
+	) {}
+
+	getMaxLength(originalList: string[]) {
+		if (originalList.length < 0) {
+			return 0;
+		}
+
+		const list = [...originalList];
+
+		return list.sort((a, b) => b.length - a.length)[0].length;
+	}
+
+	getMaxLengthsOfColumn(col: keyof T) {
+		const values = this.data.map((record) => String(record[col]));
+		return this.getMaxLength([...values, this.labels[col]]);
+	}
+
+	printTable() {
+		const maxLengths = Object.keys(this.labels).reduce((acc, col) => {
+			acc[col] = this.getMaxLengthsOfColumn(col as keyof T);
+			return acc;
+		}, {});
+
+		const headerRow = Object.keys(this.labels)
+			.map((col) => this.labels[col].padEnd(maxLengths[col]))
+			.join(" | ");
+
+		console.log(headerRow);
+
+		this.data.forEach((record) => {
+			const row = Object.keys(record)
+				.map((col) => record[col].padStart(maxLengths[col]))
+				.join(" | ");
+			console.log(row);
+		});
+	}
+}
